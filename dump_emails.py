768d036337ef526db43708d2bc41cb29d5f68689
Thouis (Ray) Jones <thouis@gmail.com>
2012-07-10 16:22:10 +0200

    generate TRAC_TO_GITHUB_USERS.txt

--- dump_emails.py
+++ dump_emails.py
@@ -1,5 +1,6 @@
-# build map of email to github username
-import sqlite3
+# build map of trac to github username
+from check_emails import get_ticket_owners, get_ticket_reporters, get_ticket_ccs
+import util
 from github import Github
 import getpass
 
@@ -8,15 +9,25 @@ p = getpass.getpass("Password: ")
 g = Github(u, p)
 del p
 
-conn = sqlite3.connect("numpy-trac.db")
-c = conn.cursor()
-outf = open("EMAIL_TO_GITHUB.txt", "w")
-badf = open("BAD_EMAILS.txt", "w")
-for (email,) in c.execute('SELECT value FROM session_attribute WHERE name="email"').fetchall():
-    try:
-        user = g.legacy_search_user_by_email(email)
-        outf.write("%s %s\n" %(email, user.login))
-        print "%s %s\n" % (email, user.login)
-    except Exception, e:
-        print "NOT FOUND", email, e
-        badf.write("%s\n" % email)
+if __name__ == '__main__':
+    outf = open("TRAC_TO_GITHUB_USERS.txt", "w")
+    names = get_ticket_owners() + get_ticket_reporters() + get_ticket_ccs()
+    import collections
+    counts = collections.Counter(names)
+    cn = [(c, n) for n, c in counts.iteritems()]
+    cn.sort()
+    cn.reverse()
+    for c, n in cn:
+        if c > 1:
+            tracemail = util.trac_user_email(n)
+            ghuser = util.trac_email_to_github(tracemail)
+            if ghuser is None and '@' in tracemail:
+                try:
+                    ghuser = g.legacy_search_user_by_email(tracemail).login
+                except:
+                    pass
+            if ghuser is not None:
+                outf.write("%s %s\n" % (n, ghuser))
+                outf.flush()
+            else:
+                print "MISSING", n, tracemail, c
